d950b8178118b8c783f50fd6974f7be51bda2fe7
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:49:55 -0700

    sorry

---
+++ 18Seeing_the_world/22International_Error/22International_Error.js
@@ -0,0 +1,11 @@
+//written by WA siddiqui.
+// 22.03.2024,
+//if you haven't recieved an array index error in one of your programs yet,try to make one happen.
+//creating  Array.
+let newArray = ["Lap Top", "smart phone", "smart watch", "Tablets"];
+//accesing Array by index.
+console.log(newArray[5]);
+//make sure you correct the error .before closing the programe
+//coreect the error.
+console.log(newArray);
+export {};
