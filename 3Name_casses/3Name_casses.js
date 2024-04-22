c51568b7f31d0683463ce9712e0dc5c04b6dfb0f
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 11:55:01 -0700

    Exercise 3 - Name Cases
    Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.

---
+++ 3Name_casses/3Name_casses.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "W.siddiqui";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toLocaleUpperCase() + personName.slice(1).toLocaleLowerCase());
---
+++ 3Name_casses/3Name_casses.ts
@@ -0,0 +1,7 @@
+let personName = "W.siddiqui";
+
+console.log(personName.toLowerCase());
+
+console.log(personName.toUpperCase());
+
+console.log(personName.charAt(0).toLocaleUpperCase()+personName.slice(1).toLocaleLowerCase());
