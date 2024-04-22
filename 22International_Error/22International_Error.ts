2d23a347c68f54210bfb85de2652d658f366ae09
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:49:09 -0700

    Exercise-22
    If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.

---
+++ 22International_Error/22International_Error.ts
@@ -0,0 +1,19 @@
+//written by WA siddiqui.
+// 22.03.2024,
+
+//if you haven't recieved an array index error in one of your programs yet,try to make one happen.
+
+//creating  Array.
+
+let newArray: string[] = ["Lap Top","smart phone","smart watch","Tablets"];
+
+//accesing Array by index.
+
+console.log(newArray[5]);
+
+//make sure you correct the error .before closing the programe.
+//coreect the error.
+
+console.log(newArray);
+
+
