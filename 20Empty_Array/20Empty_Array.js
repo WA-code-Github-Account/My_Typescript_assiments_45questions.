b23801094e1841932132e215d4a8f9bccb356499
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:43:40 -0700

    Exercise-20
    Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.

---
+++ 20Empty_Array/20Empty_Array.js
@@ -0,0 +1,14 @@
+"use strict";
+//Written by: WA Siddiqui.
+//DATE: 22-3-24
+Object.defineProperty(exports, "__esModule", { value: true });
+//Think of something you could store in array.
+let items = ["pakistan", "karachi", "urdu", "burns road"];
+//write programe that create a list containing these iteams.
+let item2 = [];
+item2.push("Pakistan");
+item2.push("India");
+item2.push("Dubai");
+item2.push("America");
+item2.push("Saudia");
+console.log(item2);
---
+++ 20Empty_Array/20Empty_Array.ts
@@ -0,0 +1,17 @@
+//Written by: WA Siddiqui.
+//DATE: 22-3-24
+
+//Think of something you could store in array.
+let items :string[] = ["pakistan","karachi","urdu","burns road"] ;
+
+//write programe that create a list containing these iteams.
+
+let item2 : string[] = [];
+item2.push("Pakistan");
+item2.push("India");
+item2.push("Dubai");
+item2.push("America");
+item2.push("Saudia")
+
+
+console.log(item2);
