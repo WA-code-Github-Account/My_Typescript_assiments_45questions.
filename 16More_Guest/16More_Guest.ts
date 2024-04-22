7dd744a7dbdb679d4bb2f2f1c2f0073202e158a1
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:29:06 -0700

    Exercise-16
    You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
    • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
    
    • Add one new guest to the beginning of your array.
    
    • Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.

---
+++ 16More_Guest/16More_Guest.ts
@@ -0,0 +1,33 @@
+let guest_list :string[] = ['sir Ameen','sir Daniyal','sir Zia','sir Hamzah'];
+
+//for (let index = 0; index < guest_list.length; index++) {
+
+ //   console.log('Respected.' + guest_list[index] + ',\n\nit is our pleasure to invite you on Dinner.\n\nThank you!\n\n\n')
+    
+//};
+
+let absent_guest :string = 'sir Ameen';
+
+let new_guest :string = 'Governor Sahab';
+
+guest_list[0] = new_guest ;
+
+for (let index = 0; index < guest_list.length; index++) {
+
+    console.log('Respected.' + guest_list[index] + ',\n\nit is our pleasure to invite you on Dinner.\n\nThank you!\n\n\n');
+
+
+    
+}
+console.log(` ${absent_guest} is not coming to the party.\n\n`);
+
+console.log('One good news ! we find one big table for Dinner we are inviting 3 more guests.');
+
+guest_list.unshift('sir Jaffar');
+guest_list.splice(2,0,'sir Faisal Eidhi');
+guest_list.push('sir Bashir Ahmed');
+
+for (let index = 0; index < guest_list.length; index++) {
+    console.log('Respected . ' + guest_list[index] + '\n\nit is our pleasure to invite you on Dinner.\n\nThank you!\n\n\n')
+    
+}
