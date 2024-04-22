9fb349ba75ee04ebc3a3edfa3ded24d5865a8e28
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 11:43:22 -0700

    Exercise 2 - Personal Message
    First Exercise personal message, printing a personalized message to user.

---
+++ 2Personal_message/2Personal_Message.js
@@ -0,0 +1,5 @@
+"use strict";
+//This save the name.
+let personName = "Warda";
+//This show the message.
+console.log('Hello', personName, "would you like to learn some typescript today?");
---
+++ 2Personal_message/2Personal_Message.ts
@@ -0,0 +1,7 @@
+//This save the name.
+
+let personName :string = "Warda";
+
+//This show the message.
+
+console.log('Hello', personName, "would you like to learn some typescript today?");
