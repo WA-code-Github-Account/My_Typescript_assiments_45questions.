0015fa27ad490463d4ae30122d6e19a4a50abc98
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:11:35 -0700

    Exercise-9
     Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ 9Favorite_Number/9Favorite_Number.js
@@ -0,0 +1,5 @@
+"use strict";
+let favoriteNumber = 7;
+//this show yours favoritenumber
+console.log(`My favoriteNumber is ${favoriteNumber} `);
+//this show favoriteNumber with message
---
+++ 9Favorite_Number/9Favorite_Number.ts
@@ -0,0 +1,5 @@
+let favoriteNumber: number = 7
+//this show yours favoritenumber
+
+console.log(`My favoriteNumber is ${favoriteNumber} `);
+//this show favoriteNumber with message
