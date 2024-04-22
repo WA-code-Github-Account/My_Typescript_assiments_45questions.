d780d1d1358113b7fcbb435ec66317fc18cd6479
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:24:27 -0700

    Exercise-14
    If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ 14Guest_List/14GuestList.ts
@@ -0,0 +1,7 @@
+let guest_list :string[] = ['Governor Sahab','sir Daniyal','sir Zia','sir Ameen'];
+
+for (let index = 0; index < guest_list.length; index++) {
+
+    console.log('Respected.' + guest_list[index] + ',\n\nit is our pleasure to invite you on Dinner.\n\nThank you!\n\n\n')
+    
+};
