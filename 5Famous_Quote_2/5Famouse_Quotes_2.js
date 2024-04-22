af307f632983093f89793a62ef7c5e1f433c6e55
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:02:41 -0700

    Exercise-5
     Repeat Exercise 4, but this time store the famous person’s name in a variable called famous person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ 5Famous_Quote_2/5Famouse_Quotes_2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famouse_person = 'Albert Einstein';
+let message = `"I have no special talent, I am only passionately curoious"`;
+console.log(`${famouse_person} once said, ${message}`);
---
+++ 5Famous_Quote_2/5Famouse_Quotes_2.ts
@@ -0,0 +1,5 @@
+let famouse_person: string ='Albert Einstein';
+
+let message: string = `"I have no special talent, I am only passionately curoious"`;
+
+console.log(`${famouse_person} once said, ${message}`);
