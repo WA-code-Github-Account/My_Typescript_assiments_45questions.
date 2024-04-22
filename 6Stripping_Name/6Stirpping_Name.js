d974b622106417bd432aeb9844eb061eb248c4fa
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:05:29 -0700

    Exercise-6
    Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.

---
+++ 6Stripping_Name/6Stirpping_Name.js
@@ -0,0 +1,7 @@
+"use strict";
+let personname = "\n\t W.siddiqui \t\n";
+//This save the name with whitespace
+console.log(personname);
+//show the name with whitespace
+console.log(personname.trim());
+//show the name without whitespace
---
+++ 6Stripping_Name/6Stirpping_Name.ts
@@ -0,0 +1,9 @@
+let personname:string= "\n\t W.siddiqui \t\n";
+//This save the name with whitespace
+
+console.log(personname);
+//show the name with whitespace
+
+console.log(personname.trim());
+//show the name without whitespace
+
