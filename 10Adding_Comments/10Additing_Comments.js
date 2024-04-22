34b59dbbc62d634a9274245105845dd33017c01d
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-22 12:15:04 -0700

    Exercise-10
    Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.

---
+++ 10Adding_Comments/10Additing_Comments.js
@@ -0,0 +1,5 @@
+"use strict";
+//Warda, 12-3-2024.
+// This programe print a personal message.
+let myName = "Warda";
+console.log(`Hello ${myName}, would you like to learn some Typescript Toaday ?`);
---
+++ 10Adding_Comments/10Additing_Comments.ts
@@ -0,0 +1,6 @@
+//Warda, 12-3-2024.
+// This programe print a personal message.
+
+let myName: string = "Warda";
+
+console.log(`Hello ${myName}, would you like to learn some Typescript Toaday ?`);
