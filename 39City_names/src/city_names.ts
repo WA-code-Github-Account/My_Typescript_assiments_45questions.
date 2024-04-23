d72ae3556f19229d5344f1de62f3a33b3fa6689a
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-23 11:31:21 -0700

    Exercise-39
    Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
    
    "Lahore, Pakistan"
    
    Call your function with at least three city-country pairs, and print the value that’s returned.

---
+++ 39City_names/src/city_names.ts
@@ -0,0 +1,12 @@
+//Written by: wa.siddiqui
+// Roll No. : 00200937
+
+// Function for city_country
+function city_country(city: string, country: string): string {
+    return `${city}, ${country}`;
+  }
+  
+  // Result
+  console.log(city_country("\nKarachi", "Pakistan."));
+  console.log(city_country("\nchicago", "USA."));
+  console.log(city_country("\nParis", "France.\n"));
