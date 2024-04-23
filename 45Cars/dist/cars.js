aa2fb8b4a2b1f9fbc0eab2609ac447769fbca318
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-23 12:09:07 -0700

    Exercise-45
     Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.

--- 45Cars/dist/cars.js
+++ 45Cars/dist/cars.js
@@ -1,7 +1,5 @@
-"use strict";
 //Written by: wa.siddiqui.
 // Roll No. : 00200937.
-Object.defineProperty(exports, "__esModule", { value: true });
 function storeCarInfo(manufacturer, modelName, ...extras) {
     const carInfo = {
         manufacturer,
@@ -12,3 +10,4 @@ function storeCarInfo(manufacturer, modelName, ...extras) {
 }
 const answer = storeCarInfo('Toyota', 'Camry', { color: 'blue' }, { features: ['navigation', 'sunroof'] });
 console.log(answer);
+export {};
