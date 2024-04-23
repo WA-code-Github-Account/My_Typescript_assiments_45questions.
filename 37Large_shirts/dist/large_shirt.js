b2e15d70f41bacf933e2160296ff15a584328a76
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-23 10:24:50 -0700

    Delete 37Large_shirts directory

--- 37Large_shirts/dist/large_shirt.js
+++
@@ -1,9 +0,0 @@
-//Written by: wa.siddiqui
-// Roll No. : 00200937
-function make_shirt(size = "Large", message = " I Love Typescript !") {
-    console.log(`\nThe size of the shirt is ${size} and the message is ${message}`);
-}
-make_shirt();
-make_shirt("Medium");
-make_shirt("Small", "I Love Javascript!\n");
-export {};
