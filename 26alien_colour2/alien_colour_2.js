9597965600723530a8a809ffa2ef3fdd88275c0c
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-23 10:01:51 -0700

    Exercise-26
    Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
    • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
    
    • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
    
    • Write one version of this program that runs the if block and another that runs the else block.

---
+++ 26alien_colour2/alien_colour_2.js
@@ -0,0 +1,20 @@
+"use strict";
+//Written by: wa.siddiqui
+//DATE: 13-4-24
+Object.defineProperty(exports, "__esModule", { value: true });
+// set colour.
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("\n\nPlayer just earned 5 ruppes  for shooting the alien!");
+}
+else {
+    console.log("\n\nPlayer just earned 10 ruppes!");
+}
+// Color Changed.
+alien_color = 'yellow';
+if (alien_color === 'green') {
+    console.log("\n\nPlayer just earned 5 ruppe for shooting the alien!\n");
+}
+else {
+    console.log("\n\nPlayer just earned 10 ruppes!");
+}
---
+++ 26alien_colour2/alien_colour_2.ts
@@ -0,0 +1,27 @@
+//Written by: wa.siddiqui
+//DATE: 13-4-24
+
+// set colour.
+
+let alien_color :string = 'green';
+
+if (alien_color === 'green') {
+    console.log("\n\nPlayer just earned 5 ruppes  for shooting the alien!");
+} 
+else 
+{
+    console.log("\n\nPlayer just earned 10 ruppes!");
+}
+
+
+// Color Changed.
+
+ alien_color = 'yellow';
+
+if (alien_color === 'green') {
+    console.log("\n\nPlayer just earned 5 ruppe for shooting the alien!\n");
+} else {
+    
+    console.log("\n\nPlayer just earned 10 ruppes!");
+
+}
